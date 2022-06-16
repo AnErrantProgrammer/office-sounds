5f69eed8aa6ba1561bef8d2537d52e963cc2f366
Sal Costa <costas0811@hotmail.com>
2022-06-15 22:56:09 -0500

    Set up the timing interval

---
+++ README.md
@@ -0,0 +1,11 @@
+## Running the app
+
+To start run `http-server -c0`
+
+## Sounds
+
+There are three types of sounds:
+
+* Background - The constant hum of the office
+* Ambient - Short clips of things happening in the office like phones ringing
+* Events - These are longer clips either happening in a conference room or in the main office. Depending on the type of event they may have additional filters applied to make it sound more realistic.
--- js/app.js
+++ js/app.js
@@ -2,8 +2,11 @@ const App = new Vue({
   el: "#app",
   data: {
     effects: {},
-    playingSound: null,
+    enableEvents: false,
     timer: null,
+    volume: 1,
+    minimumDelay: 10000,
+    lastEvent: null,
     sounds: {
       background: [
         {
@@ -17,33 +20,32 @@ const App = new Vue({
         },
       ],
 
-      conferences: [
-        {
-          id: "conference-prison-mike",
-          type: "conferences",
-          file: "prison-mike.mp3",
-          volume: 0,
-          autoplay: false,
-          loop: false,
-        },
-      ],
-
-      office: [
-        {
-          id: "office-chairs",
-          type: "office",
-          file: "chairs.mp3",
-          volume: 0.1,
-          autoplay: false,
-          loop: false,
-        },
+      event: [
+        // {
+        //   id: "conference-prison-mike",
+        //   type: "conferences",
+        //   file: "prison-mike.mp3",
+        //   volume: 0.9,
+        //   autoplay: false,
+        //   loop: false,
+        //   filters: ["lowPassFilter"],
+        // },
+        // {
+        //   id: "office-chairs",
+        //   type: "office",
+        //   file: "chairs.mp3",
+        //   volume: 0.1,
+        //   autoplay: false,
+        //   loop: false,
+        // },
         {
           id: "office-foremangrill",
           type: "office",
           file: "foremangrill.mp3",
-          volume: 0.1,
+          volume: 1,
           autoplay: false,
           loop: false,
+          filters: ["convolver"],
         },
       ],
 
@@ -52,7 +54,7 @@ const App = new Vue({
           id: "ambient-phones",
           type: "ambient",
           file: "phones.mp3",
-          volume: 1,
+          volume: 0.1,
           autoplay: false,
           loop: false,
         },
@@ -68,34 +70,72 @@ const App = new Vue({
           id: "ambient-thisispam",
           type: "ambient",
           file: "thisispam.mp3",
-          volume: 1,
+          volume: 0.2,
           autoplay: false,
           loop: false,
         },
       ],
     },
   },
-  watch: {},
+  watch: {
+    /**
+     * Link the volume property to the Pizzicato context volume
+     */
+    volume: function () {
+      Pizzicato.volume = this.volume;
+    },
+  },
   mounted: async function () {
+    let vm = this;
+    // Initialize all the effects
+    this.initializeEffects();
+
+    // Load all the sounds
     this.initializeSounds();
+
+    // Start the main loop and store it in a variable in case it needs to be destroyed
+    this.timer = setInterval(function () {
+      vm.mainLoop();
+    }, 1000);
   },
   created: function () {},
   methods: {
+    /**
+     * Handles the scroll event lowering volume on scroll down and raising it on scroll up
+     * @param {*} event
+     */
+    handleScroll: function (event) {
+      if (event.deltaY < 0) {
+        this.volume += 0.1;
+      } else {
+        this.volume -= 0.1;
+      }
+
+      // Clamp the volume between 0 and 1
+      this.volume = Math.min(Math.max(this.volume, 0), 1);
+    },
+    /**
+     * Initializes all the effects used by the sounds
+     * Note: To make importing sounds easier I just export the raw sounds and then apply the effects
+     *       Using an external program to apply the effects sounded better but was way too much effort
+     */
     initializeEffects: async function () {
       let vm = this;
+
+      // Low pass filter is used to make the audio sound if its in another room with the door closed
+      // Used for conferences
       var lowPassFilter = new Pizzicato.Effects.LowPassFilter({
         frequency: 300,
         peak: 2,
       });
 
+      // High pass filter is not used right now
       var highPassFilter = new Pizzicato.Effects.HighPassFilter({
         frequency: 300,
         peak: 10,
       });
 
-      vm.effects.lowPassFilter = lowPassFilter;
-      vm.effects.highPassFilter = highPassFilter;
-
+      // Convolver is ATTEMPTING to make the audio sound if its more distant, it doesnt work great
       const convolverPromise = new Promise((resolve, reject) => {
         var convolver = new Pizzicato.Effects.Convolver(
           {
@@ -110,41 +150,106 @@ const App = new Vue({
         );
       });
 
+      // I forget what this effect was supposed to do
       var compressor = new Pizzicato.Effects.Compressor({
         threshold: -24,
         ratio: 12,
       });
 
+      vm.effects.lowPassFilter = lowPassFilter;
+      vm.effects.highPassFilter = highPassFilter;
       vm.effects.compressor = compressor;
 
       await Promise.all([convolverPromise]);
-      console.log("All effects Loaded");
     },
+
+    /**
+     * Initializes all sounds and effects
+     */
     initializeSounds: async function () {
       let vm = this;
-      vm.initializeEffects();
+
+      // Iterate through all the sounds in the three categories
+      // For each one create a Pizzicato sound context, set some properties and apply and of the global filters
       for (const [soundType, soundGroup] of Object.entries(this.sounds)) {
         soundGroup.map(async function (sound) {
           sound.buffer = new Pizzicato.Sound({ source: "file", options: { path: `sounds/${sound.type}/${sound.file}`, volume: sound.volume } }, function () {
             sound.buffer.loop = sound.loop;
             sound.loaded = true;
             sound.buffer.volume = sound.volume;
+            sound.lastPlay = null;
 
-            if (soundType != "background") {
-              sound.buffer.addEffect(vm.effects.lowPassFilter);
-              let maxValue = 3.4;
-              let maxRange = maxValue * 2;
-              sound.buffer.masterVolume.gain.value = sound.volume * maxRange - maxValue;
+            if (sound.filters) {
+              sound.filters.forEach((filter) => sound.buffer.addEffect(vm.effects[filter]));
             }
           });
         });
       }
     },
 
+    /**
+     * This runs every tick (1 second)
+     * @returns
+     */
+    mainLoop: function () {
+      // If events arent enabled don't do anything
+      if (!this.enableEvents) return false;
+
+      // Grab the current time
+      let now = new Date();
+      let timeSinceEpoch = now.getTime();
+
+      // Check if all ambient sounds have been played
+      // if (this.sounds.ambient.every((sound) => sound.lastPlay !== null)) {
+      //   this.sounds.ambient.forEach((sound) => (sound.lastPlay = null));
+      // }
+
+      // Check if all events have been played
+      if (this.sounds.event.every((sound) => sound.lastPlay !== null)) {
+        this.sounds.event.forEach((sound) => (sound.lastPlay = null));
+      }
+
+      // Check that a minimum amount of time has elapsed since the last event
+      if (this.lastEvent == null || timeSinceEpoch - this.lastEvent > this.minimumDelay) {
+        // Find if a sound is playing
+        let soundPlaying = this.sounds.event.find((sound) => sound.buffer.playing) || this.sounds.ambient.find((sound) => sound.buffer.playing);
+
+        if (!soundPlaying) {
+          // Roll the dice to decide to play a sound
+          // If the number is above 50 play an ambient sound
+          // If the number is above 90 play an event sound (like a conference)
+          let playEvent = Math.floor(Math.random() * 100) + 1;
+
+          if (playEvent > 50) {
+            // Set the playable sounds to ambient
+            let playableSounds = this.sounds.ambient;
+
+            if (playEvent > 1) {
+              // Set the playable sounds to an event as long as it has not been played
+              playableSounds = this.sounds.event.filter((sound) => sound.lastPlay == null);
+            }
+
+            // Pick a random sound from the set
+            let randomPlayableSoundIndex = Math.floor(Math.random() * playableSounds.length);
+
+            playableSounds[randomPlayableSoundIndex].buffer.play();
+            playableSounds[randomPlayableSoundIndex].lastPlay = timeSinceEpoch;
+
+            this.lastEvent = timeSinceEpoch;
+          }
+        }
+      }
+    },
+
+    /**
+     * Turns all sounds on or off
+     */
     toggleSounds: function () {
       if (this.sounds.background[0].buffer.playing) {
+        this.enableEvents = false;
         this.sounds.background[0].buffer.stop();
       } else {
+        this.enableEvents = true;
         this.sounds.background[0].buffer.play();
       }
     },
